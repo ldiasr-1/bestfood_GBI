32b2eed0d254c44b79d3e8094ec29be2e6565b44
Romulo Martins <romulocardoso184@gmail.com>
2024-09-02 15:51:59 -0300

    Lógica Login Completa - Fazer Views.py e FrontEnd

--- accounts/models.py
+++ accounts/models.py
@@ -8,3 +8,9 @@ class CustomUser (AbstractUser):
     nome = models.CharField(max_length=100)
     sobrenome = models.CharField(max_length=100)
     
+def save (self, *args, **kwargs):
+    if self.nome:
+        self.first_name = self.nome
+    if self.sobrenome:
+        self.last_name = self.sobrenome
+    super(CustomUser, self).save(*args, **kwargs)
--- usuarios/forms.py
+++ usuarios/forms.py
@@ -4,7 +4,30 @@ from django import forms
 
 class UsarioForm(UserCreationForm):
     email = forms.EmailField(required=True)
-
+    password1 = forms.CharField(label='Senha', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirme a senha', widget=forms.PasswordInput)
     class Meta:
         model = CustomUser
         fields = ['nome', 'sobrenome', 'endereco', 'telefone', 'email', 'username', 'password1', 'password2']
+        
+def clean_password2(self):
+    password1 = self.cleaned_data.get("password1")
+    password2 = self.cleaned_data.get("password2")
+    if password1 and password2 and password1 != password2:
+        raise forms.ValidationError(
+            self.error_messages['As senhas não correspondem'],
+            code='password_mismatch',
+        )
+    return password2
+
+def save (self, commit=True):
+    user = super().save(commit=False)
+    user.set_password(self.cleaned_data["password1"])
+    if commit:
+        user.save()
+    return user
+
+class UsuarioUpdateForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ['nome', 'sobrenome', 'endereco', 'telefone', 'email']
